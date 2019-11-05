f6d486e94a0a2afdcf60f862730b9a91c2b9ed57
ayumu seki <a.s.0208@uec.ac.jp>
2019-11-05 22:44:58 +0900

    更新

--- development.js
+++ development.js
@@ -1,31 +1,34 @@
-import path from 'path'
-import HtmlWebpackPlugin from 'html-webpack-plugin' 
-const src  = path.resolve(__dirname, 'src')
-const dist = path.resolve(__dirname, 'dist')
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const src = path.resolve(__dirname, 'src');
+const dist = path.resolve(__dirname, 'dist');
 
 export default {
   mode: 'development',
-  entry: './src/js/index.jsx',
+  entry: src + '/js/index.jsx',
 
   output: {
-    path: __dirname + "/src/",
-    filename: "client.min.js"
+    path: __dirname + '/src/',
+    filename: 'client.min.js'
   },
 
   module: {
     rules: [
       {
-      test: /\.jsx?$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env']
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
           }
-        }]
+        ]
       },
-      { test: /\.css$/, loader: "style-loader!css-loader" }]
-    },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ]
+  },
 
   resolve: {
     extensions: ['.js', '.jsx']
@@ -33,8 +36,8 @@ export default {
 
   plugins: [
     new HtmlWebpackPlugin({
-        template: src + '/index.html',
-        filename: 'index.html'
-      })
+      template: src + '/index.html',
+      filename: 'index.html'
+    })
   ]
-}
+};
